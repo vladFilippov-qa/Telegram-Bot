f64b49b243c8322a6c429f120de70a0f326158cc
Владислав <116024287+vladFilippov-qa@users.noreply.github.com>
2022-11-20 12:41:26 +0300

    Update card_bot.py

--- card_bot.py
+++ card_bot.py
@@ -9,7 +9,7 @@ from telebot import TeleBot, types
 from faker import Faker
 
 
-bot = TeleBot(token='Вставить_свой_токен', parse_mode='html') # создание бота
+bot = TeleBot(token='Мой токен', parse_mode='html') # создание бота
 
 faker = Faker() # утилита для генерации номеров кредитных карт
 
